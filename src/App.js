9cbd479e806ff07bf94a97c3743ceccf63abe7b7
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-05-24 12:53:45 +0600

    Sign In With Google and Private Route Done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React, { createContext, useState } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Book from './components/Book/Book';
 import Header from './components/Header/Header';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+
+//Context API
+export const userContext = createContext();
 
 function App() {
-  return (
-      <Router>
-          <Header/>
-          <Switch>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/book/:bedType">
-              <Book />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
-      </Router>
-  );
+	const [loggedInUser, setLoggedInUser] = useState({});
+	return (
+		<userContext.Provider value={[loggedInUser, setLoggedInUser]}>
+			<p>Name: {loggedInUser.name}</p>
+			<Router>
+				<Header />
+				<Switch>
+					<Route path='/home'>
+						<Home />
+					</Route>
+					<Route path='/login'>
+						<Login />
+					</Route>
+					<PrivateRoute path='/book/:bedType'>
+						<Book />
+					</PrivateRoute>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
+		</userContext.Provider>
+	);
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import firebaseConfig from '../../firebase.config';
+import { userContext } from './../../App';
 
 const Login = () => {
+	const [loggedInUser, setLoggedInUser] = useContext(userContext);
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+	const handleGoogleSignIn = () => {
+		const provider = new GoogleAuthProvider();
+		const auth = getAuth();
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				// The signed-in user info.
+				const { displayName, email } = result.user;
+				const signedInUser = { name: displayName, email };
+				setLoggedInUser(signedInUser);
+			})
+			.catch((error) => {
+				// Handle Errors here.
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// The email of the user's account used.
+				const email = error.customData.email;
+				// The AuthCredential type that was used.
+				const credential = GoogleAuthProvider.credentialFromError(error);
+				// ...
+			});
+	};
 	return (
 		<div>
 			<h1>This is Login Component</h1>
+			<button onClick={handleGoogleSignIn}>Google Sign In </button>
 		</div>
 	);
 };
